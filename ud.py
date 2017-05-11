a2b546c5cb78dce11daac25b5378c16011d66a10
Pilate <pilate@pilate.es>
2017-05-10 20:07:37 -0700

    also take two dots

--- ud.py
+++ ud.py
@@ -7,7 +7,7 @@ def get_data(phrase):
     return requests.get("http://api.urbandictionary.com/v0/define", params={"term":phrase}).json()
 
 
-@sopel.module.rule("\\.u (.+)")
+@sopel.module.rule("\\.?\\.u (.+)")
 def phrase_lookup(bot, trigger):
     phrase = trigger.group(1)
     results = get_data(phrase)
