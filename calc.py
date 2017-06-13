cc3a5dbec60b45c3df8eed5c904b50969717894f
Pilate <pilate@pilate.es>
2017-06-12 19:58:16 -0700

    allow calcuation of coin prices

--- calc.py
+++ calc.py
@@ -1,12 +1,24 @@
 import ast
 import math
 import operator
+import re
 
 import sopel.module
 
 
 
-# Taken from http://stackoverflow.com/questions/26505420/evaluate-math-equations-from-unsafe-user-input-in-python
+try:
+    import crypto as c
+except:
+    c = None
+
+try:
+    import finance as f
+except:
+    f = None
+
+
+# Borrowed from http://stackoverflow.com/questions/26505420/evaluate-math-equations-from-unsafe-user-input-in-python
 
 operations = {
     ast.Add: operator.add,
@@ -17,6 +29,34 @@ operations = {
     ast.BitXor: operator.pow,
 }
 
+
+def coin_price(symbol):
+    if c is None:
+        raise Exception("Crypto module not found")
+
+    prices = c.get_prices()
+
+    for price in prices:
+        if symbol in [price["name"].lower(), price["symbol"].lower()]:
+            return price["price_usd"]
+
+        elif (symbol in price["name"].lower()) or (symbol in price["symbol"].lower()):
+            return price["price_usd"]
+
+    raise Exception("Crypto symbol not found")
+
+
+def stock_price(symbol):
+    if f is None:
+        raise Exception("Finance module not found")
+
+    data = f.get_data_cnbc(symbol)
+    if not data:
+        raise Exception("Failed to get symbol data")
+
+    return data[0]["last"]
+
+
 functions = {
     "acos": math.acos,
     "asin": math.asin,
@@ -25,10 +65,8 @@ functions = {
     "cos": math.cos,
     "sin": math.sin,
     "tan": math.tan,
-
     "floor": math.floor,
 }
-
 
 names = {
     "pi": math.pi,
@@ -39,7 +77,14 @@ def safe_eval(node):
     if type(node) == unicode:
         node = ast.parse(node, "<string>", "eval").body
 
-    if isinstance(node, ast.Num):
+    if isinstance(node, ast.Name):
+        node_id = str(node.id)
+        if node.id.startswith("_"):
+            return coin_price(node_id[1:])
+        else:
+            return stock_price(node_id)
+
+    elif isinstance(node, ast.Num):
         return node.n
 
     elif isinstance(node, ast.BinOp):
@@ -67,6 +112,8 @@ def safe_eval(node):
 @sopel.module.rule("\\.?\\.calc (.+)")
 def calc(bot, trigger):
     expression = trigger.group(1).strip()
+    expression = re.sub("\s", "", expression)
+
     try:
         result = safe_eval(expression)
     except Exception as e:
