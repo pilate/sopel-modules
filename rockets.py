056b79c6ef51437ff5878fb84e22bbb70efce9f7
Pilate <pilate@pilate.es>
2017-06-24 14:08:14 -0700

    rocket launch lookup

---
+++ rockets.py
@@ -0,0 +1,53 @@
+import sopel.module
+
+import requests
+
+import datetime
+import time
+
+
+
+def get_launch_data(name):
+    if name:
+        name = "/" + name
+
+    launches = requests.get("https://launchlibrary.net/1.2/launch{0}".format(name), params={
+        "mode": "verbose",
+        "startdate": datetime.datetime.utcnow().date(),
+    }).json()
+
+    if not launches["count"]:
+        return []
+
+    # Filter out launches that already happened
+    launches = filter(lambda l: l["status"] not in [3, 4], launches["launches"])
+
+    return launches
+
+
+def print_launch_data(bot, launch):
+    types = ", ".join(map(lambda l: l["typeName"], launch["missions"]))
+
+    line = "(Rocket: {name}) (Payload Type: {type}) (Where: {location}) (When: {net})".format(
+        name=launch["rocket"]["name"],
+        type=types,
+        location=launch["location"]["name"],
+        net=launch["net"])
+
+    # If we have a launch time, calculate the time remaining
+    if launch["netstamp"]:
+        delta = datetime.timedelta(seconds=launch["netstamp"] - int(time.time())) 
+        line += " (Countdown: {delta})".format(delta=delta)
+
+    bot.say(line)
+
+
+@sopel.module.rule("\\.?\\.launch(?: (.+))?$")
+def next_launch(bot, trigger):
+    name = trigger.group(1) or ""
+
+    data = get_launch_data(name.strip())
+    if data:
+        print_launch_data(bot, data[0])
+    else:
+        bot.say("No launches found.")
