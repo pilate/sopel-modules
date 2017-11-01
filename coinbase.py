9f12a0c73927a7ff9e7adaad97a562b482c456e7
Pilate <pilate@pilate.es>
2017-11-01 16:45:26 -0700

    add gdax lookup

--- coinbase.py
+++ coinbase.py
@@ -7,7 +7,7 @@ import sopel.module
 PAIRS = ["BTC-USD", "ETH-USD", "LTC-USD"]
 
 
-def get_prices():
+def get_prices_coinbase():
     prices = []
     for pair in PAIRS:
         response = requests.get("https://api.coinbase.com/v2/prices/{0}/spot".format(pair), headers={
@@ -17,13 +17,36 @@ def get_prices():
     return prices
 
 
-@sopel.module.rule("\\.?\\.(cr[yi]pto|co[ir]nbase)$")
+def get_products_gdax():
+    return requests.get("https://api.gdax.com/products").json()
+
+
+def get_price_gdax(product_id):
+    return requests.get("https://api.gdax.com/products/{0}/ticker".format(product_id)).json()
+
+
+@sopel.module.rule("\\.?\\.(cr[yi]pto|co[ir]nbas?e)$")
 @sopel.module.rule("\\.?\\.corn$")
 def cb_lookup(bot, trigger):
-    prices = get_prices()
+    prices = get_prices_coinbase()
 
     texts = []
     for pair, price in prices:
         texts.append("{coin}: ${price}".format(coin=pair.split("-")[0], price=price))
 
     bot.say("Coinbase - {0}".format(", ".join(texts)))
+
+
+@sopel.module.rule("\\.?\\.gd(ax)?e?$")
+def gdax_lookup(bot, trigger):
+    products = filter(lambda p: "USD" in p["id"], get_products_gdax())
+
+    texts = []
+    for product in products:
+        price = get_price_gdax(product["id"])
+        texts.append("{market}: ${price:,} ({volume:,})".format(
+            market=product["id"], 
+            price=float(price["price"]),
+            volume=int(float(price["volume"]))))
+
+    bot.say("GDAX - {0}".format(", ".join(texts)))
