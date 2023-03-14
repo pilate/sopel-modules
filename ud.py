762d0391381570a0ffa014a2fb0cb273268e3c73
Pilate <noptical@gmail.com>
2023-03-13 20:51:29 -0700

    update urban dictionary

--- ud.py
+++ ud.py
@@ -2,16 +2,41 @@ import sopel.module
 import requests
 
 
-
 def get_data(phrase):
-    return requests.get("http://api.urbandictionary.com/v0/define", params={"term":phrase}).json()
+    return requests.get(
+        "http://api.urbandictionary.com/v0/define", params={"term": phrase}, timeout=10
+    ).json()
 
 
 @sopel.module.rule("\\.?\\.u (.+)")
 def phrase_lookup(bot, trigger):
     phrase = trigger.group(1)
     results = get_data(phrase)
+    print(results)
     if not results.get("list"):
+        return
+
+    if phrase.lower() in [
+        "suki (tm)",
+        "suki(tm)",
+        "the new world religion (tm)",
+        "the new world religion(tm)" "\xc3\x89lite",
+    ]:
+        bot.say("RIP Pitz")
+        return
+
+    if phrase.lower() in [
+        "vip (tm)",
+        "vip(tm)",
+        "superelite",
+        "superelite(tm)",
+        "superelite (tm)",
+    ]:
+        bot.say("RIP Pitz")
+        return
+
+    if phrase.lower() in ["profit (tm)", "profit(tm)"]:
+        bot.say("RIP Pitz")
         return
 
     bot.say(results["list"][0]["definition"])
